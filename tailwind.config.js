ca9f4b7159f97636f1f6a63a4ad1d818b4e2a7d0
0706rayking-wq <0706rayking@gmail.com>
2026-05-10 01:04:51 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          red: 'rgb(var(--brand-red) / <alpha-value>)',
+          dark: 'rgb(var(--brand-red) / <alpha-value>)',
+          light: '#f8fafc'
+        }
+      },
+      fontFamily: {
+        sans: ['Noto Sans TC', 'sans-serif'],
+        serif: ['Noto Serif TC', 'serif'],
+      }
+    }
+  },
   plugins: [],
 }
